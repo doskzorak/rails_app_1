f28407c4ba4950a8df088dad56a4b474808ef7f9
doskzorak <tgmartin1985@live.com>
2019-12-27 23:23:03 -0500

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,19 @@
+class TagsController < ApplicationController
+  def show
+    @tags = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tags = Tag.find(params[:id])
+    @tags.destroy
+
+    flash.notice = "Tag '#{@article.title}' Destroyed!"
+
+    redirect_to article_path(@article)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles do
     resources :comments
-  end# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  resources :tags
+  
 end
